90fbf3baaa9a8c9aa266d7d938355e089111ae16
asraful196 <ahamedsunny14@gmail.com>
2021-03-07 19:17:53 +0600

    http request,send data to the server,http post method

---
+++ http-server-get-post.js
@@ -0,0 +1,8 @@
+/// :24-6: http request ,status code , network tab bad API...
+// :24-7: send data to the server ,http post method...
+
+//http request method::
+https://www.w3schools.com/tags/ref_httpmethods.asp
+
+//error status code::
+https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
